8a7bbf361795aa2b4f93c018595ead9861e77f13
Umairlari786 <uamir.lari1998@gmail.com>
2023-05-06 02:35:45 +0530

    updated code

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ const generatePassword = () => {
     }
   }
   console.log(password);
-  document.getElementById("pass-box").innerText = password
+  document.getElementById("pass-box").innerText = truncateString(password, totalChar.value);
 }
 
 const changeColor = () => {
